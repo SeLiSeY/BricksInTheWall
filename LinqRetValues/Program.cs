c807b136ac6aeaa5797301f8f0cb1eba4d2b2452
SeLiSeY <selisey@mail.ru>
2013-07-14 22:48:00 +0700

    Начальный вариант примера

---
+++ LinqRetValues/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqRetValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with LINQ Transformations *****\n");
+            IEnumerable<string> subset = GetStringSubset();
+            foreach (string item in subset) { Console.WriteLine(item);  }
+            Console.ReadLine();
+        }
+
+        static IEnumerable<string> GetStringSubset()
+        {
+            string[] colors = { "Light Red", "Green", "Yellow", "Dark Red", "Red", "Rurple" };
+
+            // Обратитк внимание, что subset представляет собой объект, совместимый с IEnumerable<string>
+            IEnumerable<string> theRedColors = from c in colors where c.Contains("Red") select c;
+            return theRedColors;
+        }
+    }
+}
